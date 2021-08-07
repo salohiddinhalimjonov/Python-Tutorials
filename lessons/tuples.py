49be6feda389881bbdd3df616a44114cc52cb937
MyGitHubAccount <00009998id@gmail.com>
2021-08-07 10:29:03 +0500

    making changes

--- lessons/tuples.py
+++ lessons/tuples.py
@@ -43,7 +43,8 @@ print(t[::-1])
 #Tuples are unchangeable, or immutable as it also is called.
 
 #But there is a workaround. You can convert the tuple into a list, change the list,
-#and convert the list back into a tuple.
+#and convert the list back into a tuple. 
+# . 
 
 x = ("apple", "banana", "cherry")
 y = list(x)
