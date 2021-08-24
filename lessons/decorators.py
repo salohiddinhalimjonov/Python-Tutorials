8c944a387620feb33ab334871b2a3d0cf24d9e26
MyGitHubAccount <00009998id@gmail.com>
2021-08-24 18:14:38 +0500

    decorator

---
+++ lessons/decorators.py
@@ -0,0 +1,1 @@
+#This is a decorator lesson
--- lessons/functions.py
+++ lessons/functions.py
@@ -199,3 +199,31 @@ fn(1, 2, x=10, y=20)
 #(1, 2)
 #{'x': 10, 'y': 20}     
 
+
+
+#                               --PARTIAL FUNCTIONS--
+
+
+
+def multiply(a, b):
+    return a*b
+
+
+def double(a):
+    return multiply(a, 2)
+
+
+result = double(10)
+print(result)  # 20
+#As you can see, the double function reduces the arguments of the multiply function.
+
+#The double function freezes the second argument of the multiply function, which results in a new function with a simpler signature.
+
+#In other words, double function reduces the complexity of the multiply function.
+
+#In Python, the double function is called a partial function.
+
+#In practice, you use partial functions when you want to reduce the number of arguments of a function to simplify the function’s signature.
+
+#Since you’ll create partial functions sometimes, Python provides you with the partial function from the functools standard module to help you define partial functions more easily.
+
